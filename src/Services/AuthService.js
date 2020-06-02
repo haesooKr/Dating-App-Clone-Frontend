057e73d850d5bc2f13a5db551f30feab78327e6c
haesooKr <gotn676@gmail.com>
2020-06-02 17:50:29 -0400

    Create AuthService.js

---
+++ src/Services/AuthService.js
@@ -0,0 +1,37 @@
+export default {
+  login: (user) => {
+    return fetch("/user/login", {
+      method: "post",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      if (res.status !== 401) return res.json().then((data) => data);
+      else return { isAuthenticated: false, user: { username: "", role: "" } };
+    });
+  },
+  register: (user) => {
+    return fetch("/user/register", {
+      method: "post",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => data);
+  },
+  logout: () => {
+    return fetch("/user/logout")
+      .then((res) => res.json())
+      .then((data) => data);
+  },
+  isAuthenticated: () => {
+    // 리액트가 꺼져도 인증 세션이 사라지지않도록
+    return fetch("/user/authenticated").then((res) => {
+      if (res.status !== 401) return res.json().then((data) => data);
+      else return { isAuthenticated: false, user: { username: "", role: "" } };
+    });
+  },
+};
