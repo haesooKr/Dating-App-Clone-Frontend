20be1e2dc2bada8246087139339ca1a8d76c4d7c
haesooKr <gotn676@gmail.com>
2020-06-02 23:36:45 -0400

    Add imageURL state

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -5,12 +5,14 @@ export const AuthContext = createContext();
 
 export default ({ children }) => {
   const [user, setUser] = useState(null);
+  const [imageURL, setImageURL] = useState('');
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     AuthService.isAuthenticated().then(data => {
       setUser(data.user);
+      setImageURL(data.user.picture ? `/image/show/${data.user.picture}` : '');
       setIsAuthenticated(data.isAuthenticated);
       setIsLoaded(true);
     });
@@ -19,7 +21,7 @@ export default ({ children }) => {
   return (
     <div>
       {!isLoaded ? <h1>Loading</h1> : 
-      <AuthContext.Provider value={{ user, setUser, isAuthenticated, setIsAuthenticated }}>
+      <AuthContext.Provider value={{ user, setUser, isAuthenticated, setIsAuthenticated, imageURL, setImageURL }}>
         { children }
       </AuthContext.Provider>}
     </div>
