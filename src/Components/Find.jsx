e209af5525a82ed5fa5f9870d9a40b83cf028d04
haesooKr <gotn676@gmail.com>
2020-06-02 23:38:46 -0400

    Create Find component

---
+++ src/Components/Find.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import { AuthContext } from "../Context/AuthContext";
+
+const Find = () => {
+  const [people, setPeople] = useState([]);
+
+  async function findPeople() {
+    await axios.get("/user/people").then((data) => {
+      console.log(data.data.people)
+      setPeople(data.data.people);
+    });
+  }
+
+  useEffect(() => {
+    findPeople();
+  }, []);
+
+  const onClick = (e) => {
+    if(people.length > 0){
+      async function action(type){
+        await axios.post(`/user/${type}`, {
+          _id: people[0]._id,
+          firstName: people[0].firstName
+        })
+      }
+      action(e.target.value);
+
+      setPeople(people.slice(1));
+    }
+  }
+
+  const User = () => {
+    const user = people[0];
+    return (
+      <div className="user">
+        <img src={`image/show/${user.picture}`} alt="profile"></img>
+        <div className="name">{user.firstName} {user.lastName}</div>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      {people.length >= 1 ? <User /> : <div>No More User</div>}
+
+      <button onClick={onClick} value="like">Like</button>
+      <button onClick={onClick} value="dislike">Dislike</button>
+      <button onClick={onClick} value="superlike">SuperLike</button>
+    </div>
+  );
+};
+
+export default Find;
