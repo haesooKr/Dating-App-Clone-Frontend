afec4dfee773deaee5f86d40dd0f6f69807d3888
haesooKr <gotn676@gmail.com>
2020-06-04 14:30:41 -0400

    Add NavBar.scss

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,30 +2,35 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import AuthService from '../Services/AuthService';
 import { AuthContext } from '../Context/AuthContext';
+import './scss/NavBar.scss';
 
 const NavBar = (props) => {
   const { setUser, isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
 
   const onClickLogout = () => {
-    AuthService.logout().then(data => {
-      if(data.success){
-        setUser(data.user);
-        setIsAuthenticated(false);
-      }
-    })
+    const logout = window.confirm("Are you sure you want to logout?");
+    console.log(logout);
+    if(logout){
+      AuthService.logout().then(data => {
+        if(data.success){
+          setUser(data.user);
+          setIsAuthenticated(false);
+        }
+      })
+    }
   }
 
   const unauthenticatedNavBar = () => {
     return (
       <>
         <Link to="/">
-          <li className="nav-link">Home</li>
+          <li className="nav-link"><i className="fas fa-user"></i></li>
         </Link>
         <Link to="/login">
-          <li className="nav-link">Login</li>
+          <li className="nav-link"><i className="fas fa-sign-in-alt"></i></li>
         </Link>
         <Link to="/register">
-          <li className="nav-link">Register</li>
+          <li className="nav-link"><i className="fas fa-plus"></i></li>
         </Link>
       </>
     )
@@ -35,16 +40,16 @@ const NavBar = (props) => {
     return (
       <>
         <Link to="/">
-          <li className="nav-link">Home</li>
+          <li className="nav-link"><i className="fas fa-user"></i></li>
         </Link>
         <Link to="/find">
-          <li className="nav-link">Find</li>
+          <li className="nav-link"><i className="fas fa-users"></i></li>
         </Link>
         <Link to="/matches">
-          <li className="nav-link">Matches</li>
+          <li className="nav-link"><i className="fab fa-gratipay"></i></li>
         </Link>
         <button type="button" className="btn-logout" onClick={onClickLogout}>
-          Logout
+        <i className="fas fa-sign-out-alt"></i>
         </button>
       </>
     )
