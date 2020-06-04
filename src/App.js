67ef83350ccd9f1b86f7256028cef4d9968af90c
haesooKr <gotn676@gmail.com>
2020-06-03 21:15:47 -0400

    import all components & hocs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import {
+  NavBar,
+  Home,
+  Login,
+  Find,
+  Matches,
+  Room,
+  Profile,
+  Delete,
+  Register,
+} from "./Components";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import { UnPrivateRoute, PrivateRoute } from './hocs'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Route exact path="/" component={Home} />
+      <UnPrivateRoute path="/login" component={Login} />
+      <UnPrivateRoute path="/register" component={Register} />
+      <PrivateRoute path="/find" roles={["user"]} component={Find} />
+      <PrivateRoute path="/matches" roles={["user"]} component={Matches} />
+      <PrivateRoute path="/profile" roles={["user"]} component={Profile} />
+      <PrivateRoute path="/delete" roles={["user"]} component={Delete} />
+      <PrivateRoute path="/room/:roomId" roles={["user"]} component={Room} />
+    </Router>
   );
 }
 
